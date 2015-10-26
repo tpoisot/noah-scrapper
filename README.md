e5c3be1aee774979452d827ee3eed2eeb518baa0
Timothée Poisot <tim@poisotlab.io>
2015-10-26 14:24:31 -0400

    Proof of concept working

--- README.md
+++ README.md
@@ -5,3 +5,36 @@
 ~~~
 npm install
 ~~~
+
+## Run
+
+~~~
+node noah.js
+~~~
+
+Will output
+
+~~~
+[tpoisot@adele noah]$ node noah.js
+Getting data for 1892586003
+	Sci. name: Porzana carolina
+	Location:  -80.8,28.53
+~~~
+
+And a `JSON` blurb (in GeoJSON format):
+
+~~~ json
+{
+ "type": "Feature",
+ "geometry": {
+  "type": "Point",
+  "coordinates": [
+   -80.8,
+   28.53
+  ]
+ },
+ "properties": {
+  "name": "Porzana carolina"
+ }
+}
+~~~
---
+++ noah.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var colors = require('colors');
+
+var spotting_id = "1892586003";
+console.log("Getting data for".cyan.bold + " " + spotting_id)
+
+var url = "http://www.projectnoah.org/spottings/" + spotting_id;
+
+function get_spotting(error, response, html) {
+  if(!error) {
+
+    // We get the whole html page as text
+    var $ = cheerio.load(html);
+
+    // Variables of interest
+    var scientific_name, coordinates, date;
+
+    // Scientific name
+    // This one is easy since there is a single tag
+    $('.scientific_name').filter(function(){
+      var data = $(this);
+      scientific_name = data.text();
+      console.log("\t" + "Sci. name: ".blue.bold + scientific_name)
+    })
+
+    // Latitude and longitude
+    $('.spotting-map-wrapper').filter(function(){
+      var loc_bits = $(this).children().last().text().split(" ");
+      var latitude = parseFloat(loc_bits[1]);
+      var longitude = parseFloat(loc_bits[3]);
+      coordinates = [longitude, latitude]
+      console.log("\t" + "Location:  ".blue.bold + coordinates)
+    })
+
+    // We then return everything as a GEOJson object
+    var point = {"type": "Point", "coordinates": coordinates}
+    var properties = {"name": scientific_name}
+    var json = {"type": "Feature", "geometry": point, "properties": properties}
+    console.log(JSON.stringify(json, null, " "));
+  }
+}
+
+request(url, get_spotting)
