c00230f0873bdf3d63b419139ed02f78bfd6e1fd
Timothée Poisot <tim@poisotlab.io>
2015-10-28 16:29:28 -0400

    padding of keys and don't try collected IDs

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 ALL: id.txt
 
-id.txt:
-	node get_ids.js >> id.txt
-	cat id.txt | sort | uniq > aa.txt
-	mv aa.txt id.txt
+id.txt: possible_ids.js
+	node $< >> $@
 
 collect: id.txt
 	cat id.txt | xargs -P 3 -I % node noah.js %
+
+refine: id.txt
+	ls spottings/*json | cut -d/ -f 2 | cut -d. -f 1 > got.txt
+	cat {got.txt,id.txt} | sort -u | sort -R > _id.txt
+	mv _id.txt id.txt
+	rm got.txt
--- possible_ids.js
+++ possible_ids.js
@@ -1,4 +1,4 @@
-var major_start = 1000;
+var major_start = 500;
 var major_end = 1901;
 var minor_start = 00;
 var minor_end = 99;
@@ -7,11 +7,9 @@ var trail = [6000, 6001, 6002, 6003, 6004, 6005, 6006, 6007, 6008, 6009];
 for (var major = major_start; major < major_end; major++) {
   for (var minor = minor_start; minor < minor_end; minor++) {
     for (var k = 0; k < trail.length; k++) {
-      var padded_minor = '';
-      if (minor < 10) {
-        padded_minor = '0';
-      }
-      var id = major.toString() + padded_minor + minor.toString() + trail[k].toString();
+      var maj = ("000000" + major.toString()).slice(-4);
+      var min = ("00" + minor.toString()).slice(-2);
+      var id = maj + min + trail[k].toString();
       console.log(id);
     }
   }
