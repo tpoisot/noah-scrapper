e04925725fd59bf7ffdf0cc541d63742552d170d
Timothée Poisot <tim@poisotlab.io>
2015-10-28 14:39:18 -0400

    Update IDs

--- possible_ids.js
+++ possible_ids.js
@@ -1,5 +1,5 @@
-var major_start = 1880;
-var major_end = 1999;
+var major_start = 1000;
+var major_end = 1901;
 var minor_start = 00;
 var minor_end = 99;
 var trail = [6000, 6001, 6002, 6003, 6004, 6005, 6006, 6007, 6008, 6009];
@@ -11,7 +11,7 @@ for (var major = major_start; major < major_end; major++) {
       if (minor < 10) {
         padded_minor = '0';
       }
-      var id = major.toString() + padded_minor +minor.toString() + trail[k].toString();
+      var id = major.toString() + padded_minor + minor.toString() + trail[k].toString();
       console.log(id);
     }
   }
